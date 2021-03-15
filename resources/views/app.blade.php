423ad7c16d4c2a68d3d4f4fb5f8e5fa1fcc5e2f0
Clinton1998 <clintontapialagar@gmail.com>
2021-03-15 16:40:06 -0500

    google-site-verification

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="google-site-verification" content="" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Programador web - Clinton Tapia</title>
     <link rel="canonical" href="{{ Request::fullUrl() }}">
